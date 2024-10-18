3fa32a3d5a2f5cfed8ba8237f9d334881a5c33cf
hjyoonyo <hjyoon2048@gmail.com>
2024-10-18 21:00:23 +0900

    first-complete

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // 1. 기본 세팅
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const indexRouter = require('./routes/index');
-
+require('dotenv').config();
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+console.log("mongouri", MONGODB_URI_PROD);
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api",indexRouter);
 
 
-const mongoURI = 'mongodb://localhost:27017/todo-demo';
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose.connect(mongoURI,{useNewUrlParser:true}) //요즘 형태의 주소도 잘 쓰도록
 .then(()=>{ 
